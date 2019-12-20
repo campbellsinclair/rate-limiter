5a2680c6881a7479269da118b1bd6427f279c7c1
Campbell Sinclair <8000946+campbellsinclair@users.noreply.github.com>
2019-12-20 17:40:26 +1100

    Replace list with deque.

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,18 +1,19 @@
+import collections
 import time
 
 
 class RateLimiter:
     def __init__(self, max_calls, period):
         self.period = period
-        self.call_times = [0] * max_calls  # oldest at start, newest at end
-    
+        self.call_times = collections.deque([0] * max_calls)  # oldest at start, newest at end
+
     def __call__(self, func, *args, **kwargs):
-        oldest_time = self.call_times[0]
+        oldest_time = self.call_times.popleft()
         sleep_time = max(self.period - (time.time() - oldest_time), 0)
         
         time.sleep(sleep_time)
         
         return_value = func(*args, **kwargs)
-        self.call_times = self.call_times[1:] + [time.time()]
+        self.call_times.append(time.time())
         
         return return_value
